788731088fd0ddb7ed38cd42bf3f215bae578d5c
abdamah <abdamoh1@gmail.com>
2024-09-25 14:59:17 +0300

    Initial commit.

---
+++ app/page.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { CreateUser } from "./actions";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardDescription>
+            Enter your information to sign up an account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="grid gap-4" action={CreateUser}>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="first-name">First name</Label>
+                <Input id="first-name" placeholder="First Name" />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="last-name">Last name</Label>
+                <Input id="last-name" placeholder="Last Name" />
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="demo@mail.com" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+            <Button type="submit" className="w-full">
+              Create an account
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
